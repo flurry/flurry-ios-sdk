466307a41b149a3f2f7426a4ea8dc196f4679cb9
Akshay Bhandary <akshayb@yahoo-inc.com>
2016-02-20 22:27:48 -0800

    7.5.1

--- Flurry API Documentation/dir_2c12105386bcc5c3935feab96f296a41.js
+++ Flurry API Documentation/dir_2c12105386bcc5c3935feab96f296a41.js
@@ -1,4 +1,4 @@
 var dir_2c12105386bcc5c3935feab96f296a41 =
 [
-    [ "Flurry-iOS-7.5.0", "dir_e2f9d9dceb24aca6f62b6a096d58ea09.html", "dir_e2f9d9dceb24aca6f62b6a096d58ea09" ]
+    [ "Flurry.Release.7.5.1", "dir_9a619d92b46b26453b99417c3ee94ff3.html", "dir_9a619d92b46b26453b99417c3ee94ff3" ]
 ];
---
+++ Flurry API Documentation/dir_bfa9a6336bcb43b7ef722913412a6857.js
@@ -0,0 +1,5 @@
+var dir_bfa9a6336bcb43b7ef722913412a6857 =
+[
+    [ "FlurryAdError.h", "_flurry_ad_error_8h.html", "_flurry_ad_error_8h" ],
+    [ "FlurryAdTargeting.h", "_flurry_ad_targeting_8h_source.html", null ]
+];
