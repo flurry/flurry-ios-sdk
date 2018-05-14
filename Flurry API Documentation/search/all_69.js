038a815b5a9ebbd08cc680873c3c326d49cda47e
Raj Kaur <rajkaur@oath.com>
2018-05-14 14:32:33 -0700

    8.6.1

--- Flurry API Documentation/search/all_69.js
+++ Flurry API Documentation/search/all_69.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['iflurrytumblrshareparameters_2dp',['IFlurryTumblrShareParameters-p',['../protocol_i_flurry_tumblr_share_parameters-p.html',1,'']]],
   ['initialize_3a',['initialize:',['../interface_flurry_ads.html#a629c9038b83ff44baf0ad8223c89d218',1,'FlurryAds']]],
-  ['initwithgdprscope_3aandconsentstrings_3a',['initWithGDPRScope:andConsentStrings:',['../interface_flurry_consent.html#a373eeff9023480414c93da105265378e',1,'FlurryConsent']]],
+  ['initwithgdprscope_3aandconsentstrings_3a',['initWithGDPRScope:andConsentStrings:',['../interface_flurry_consent.html#aa8b918945304a9195a4a07f5b2ab7bf9',1,'FlurryConsent']]],
   ['initwithspace_3a',['initWithSpace:',['../interface_flurry_ad_banner.html#a2437af7b3137a2b8dc74971b45442ee5',1,'FlurryAdBanner::initWithSpace:()'],['../interface_flurry_ad_interstitial.html#aad64e5430b9acfefb23c5f0cb31ab1de',1,'FlurryAdInterstitial::initWithSpace:()'],['../interface_flurry_ad_native.html#a661aa88c4a33ce7220fb10326202589e',1,'FlurryAdNative::initWithSpace:()']]],
   ['isflurrymsg_3a',['isFlurryMsg:',['../interface_flurry_messaging.html#a7d9394746831a88efeb67121026ad69c',1,'FlurryMessaging']]],
   ['isvideoad',['isVideoAd',['../interface_flurry_ad_native.html#aeee0bc05028d7ca2d5272508294a1c92',1,'FlurryAdNative']]]
